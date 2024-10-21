a667c54c5f6200236131b8bf7f94c174dfd293b5
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-10-21 17:53:16 +0200

    ex 4

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,10 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+const hello = (name) => console.log(`Ciao ${name}`) 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+hello(name)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
