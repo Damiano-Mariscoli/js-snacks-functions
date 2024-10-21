694de256fdd974abe2787dbe862840b661927563
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-10-21 17:11:46 +0200

    ex 1

---
+++ 01 - Iniziali/script.js
@@ -0,0 +1,18 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+const firstChar = (names) => { 
+    for (let i = 0 ; i < names.length; i++ ){
+        let newChar = names[i][0];
+        console.log(newChar)
+    }
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+firstChar(names)
+
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
